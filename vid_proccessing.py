5708752b7011e347096bf70ad05693febd5761f2
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2021-08-15 21:16:09 +0300

    Processing videos

--- vid_proccessing.py
+++ vid_proccessing.py
@@ -1,23 +1,57 @@
+import os
+
 import pyttsx3
 import hashlib
 from moviepy.editor import *
+import requests
+import shutil
+import uuid
+import cv2
+
+GAPI_KEY = 'AIzaSyAI52k1NjCXVyJTnDuWAC1b94BCAnit7NQ'
 
 
+def get_img(text, u_uid):
+    js = requests.get(
+        f'https://www.googleapis.com/customsearch/v1?q={text}&cx=448c512f7ed8a5a3d&key={GAPI_KEY}&lr=lang_ru&searchType=image').json()
+    r = requests.get(js['items'][0]['link'], stream=True)
+    if r.status_code == 200:
+        with open(f'{u_uid}/img_buf.jpg', 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
 
-# engine = pyttsx3.init()
-# engine.say("Привет как твои дела?")
-# engine.runAndWait()
-def create_audio(text):
+
+def create_audio(text, u_uid):
     file_name = hashlib.sha256(text.encode(encoding='utf-8')).hexdigest() + '.mp3'
     engine = pyttsx3.init()
-    engine.save_to_file(file_name, f'./audio/{file_name}')
+    engine.save_to_file(text, f'{u_uid}/audio/{file_name}')
     engine.runAndWait()
-    return file_name
+    return f'{u_uid}/audio/{file_name}'
 
-def create_vid(img_path):
-    clip = ImageClip(img_path).set_duration(2)
+
+def create_vid(u_uid, audio_path):
+    audio = AudioFileClip(audio_path)
+    print(audio.duration)
+    image = cv2.imread(f'{u_uid}/img_buf.jpg')
+    clip = ImageClip(image).set_duration(audio.duration).set_audio(audio)
+    # clip.set_audio(audio)
     return clip
 
 
+def concatenate(phrase):
+    clips = []
+    u_uid = './users/' + str(uuid.uuid4())
+    os.mkdir(u_uid)
+    os.mkdir(u_uid + '/audio')
+    for text in phrase.split(' '):
+        get_img(text, u_uid)
+        clips.append(create_vid(u_uid=u_uid, audio_path=create_audio(text, u_uid)))
+    print(clips)
+    concat_clip = concatenate_videoclips(clips, method="compose")
+    concat_clip.write_videofile("test.mp4", fps=24)
+
+
 if __name__ == '__main__':
-    print(create_audio('привеет'))
+    # create_vid('asfaf', create_audio('привет как твои дела?'))
+    concatenate('Мишка косолапый по лесу идёт')
+    # print(create_audio('привеет'))
